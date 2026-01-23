a931d27016858a498e5b872ea2d38e370a2b5bd0
HundredVisionsGuy <106779600+Anonamous13243@users.noreply.github.com>
2026-01-23 09:53:08 -0800

    Added dictionary

---
+++ src/playlistinator/main.py
@@ -0,0 +1,122 @@
+input(print("Welcome to the Playlistinator! " \
+\
+"File Directory:" \
+""))
+def main_genres():
+	str("blues")
+    str("classical")
+    str("country")
+    str("electronic")
+    str("folk")
+    str("hip_Hop")
+    str("jazz")
+    str("reggae")
+    str("rock")
+
+def blues_subclasses():
+	str("country_blues")
+    ("delta_blues")
+    ("country")
+    ("piedmont_blues")
+    "chicago_blues")
+    "texas_blues")
+    "west_coast_blues")
+    "british_blues")
+    "electric_blues")
+
+def classical_subclasses():
+	"medival")
+    "renaissance")
+    "baroque")
+    "classical")
+    "romantic")
+    "modernism")
+    "impressionism")
+    "minimalism")
+    "electronic classical")
+
+Function country_subclasses():
+	"love")
+    "hard_times")
+    "raisin’_hell")
+    "nostalgia")
+    "musical_aspirations")
+    "god/religion")
+    "the_south")
+    "america")
+    "modes_of_transportation")
+
+def electronic_subclasses():
+    "electro")
+    "dubstep")
+    "techno")
+    "synth_pop")
+    "vaporwave")
+    "electroacoustic")
+    "breakbeat")
+    "industrial")
+    "breakbeat")
+
+def folk_subclasses():
+    "bluegrass")
+    "gospel")
+    "old_time")
+    "jug_band")
+    "appalachian")
+    "blues_folk")
+    "cajun")
+    "native_american")
+
+def hip_hop_subclasses():
+    "lofi")
+    "trap")
+    "phonk")
+    "freestyle_rap")
+    "latin_hip")
+    "christian")
+    "crunk")
+    "gangster_rap")
+
+def jazz_subclasses():
+    "ragtime"
+    "dixieland"
+    "swing"
+    "bebop"
+    "cool_jazz"
+    "hard_bop"
+    "modal"
+    "latin_jazz"
+    "fusion"
+
+def reggae_subclasses():
+    "roots"
+    "dub"
+    "dancehall"
+    "lovers"
+    "raggamuffin"
+    "rock_ragg"
+    "reggae_fusion"
+    "two_tone"
+    "raggaeton"
+
+def reggae_subclasses():
+    "roots"
+    "dub"
+    "dancehall"
+    "lovers"
+    "raggamuffin"
+    "rock_ragg"
+    "reggae_fusion"
+    "two_tone"
+    "raggaeton"
+
+def rock_subclasses():
+    "hardcore"
+    "classic_rock"
+    "punk_rock"
+    "alternative"
+    "indie"
+    "metal"
+    "folk_rock"
+    "progressive"
+    "electronic_rock"

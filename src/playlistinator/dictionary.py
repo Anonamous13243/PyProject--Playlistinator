1eceeec53e53ecafbcd878bf3d78d2bb144f9741
MagicG5509 <106779600+Anonamous13243@users.noreply.github.com>
2026-01-25 06:33:54 +0000

    changed variable structure to integer serials

--- src/playlistinator/dictionary.py
+++ src/playlistinator/dictionary.py
@@ -1,119 +1,109 @@
 
 
 def main_genres():
-	"blues"
-    "classical"
-    "country"
-    "electronic"
-    "folk"
-    "hip_Hop"
-    "jazz"
-    "reggae"
-    "rock"
+	blues = int(1)
+    classical = int(2)
+    country = int(3)
+    electronic = int(4)
+    folk = int(5)
+    hip_Hop = int(6)
+    jazz = int(7)
+    reggae = int(8)
+    rock = int(9)
 
 def blues_subclasses():
-	"country_blues"
-    "delta_blues"
-    "country"
-    "piedmont_blues"
-    "chicago_blues"
-    "texas_blues"
-    "west_coast_blues"
-    "british_blues"
-    "electric_blues"
+	country_blues = int(1)
+    delta_blues = int(2)
+    country = int(3)
+    piedmont_blues = int(4)
+    chicago_blues = int(5)
+    texas_blues = int(6)
+    west_coast_blues = int(7)
+    british_blues = int(8)
+    electric_blues = int(9)
 
 def classical_subclasses():
-	"medival"
-    "renaissance"
-    "baroque"
-    "classical"
-    "romantic"
-    "modernism"
-    "impressionism"
-    "minimalism"
-    "electronic classical"
+	medival = int(1)
+    renaissance = int(2)
+    baroque = int(3)
+    classical = int(4)
+    romantic = int(5)
+    modernism = int(6)
+    impressionism = int(7)
+    minimalism = int(8)
+    electronic 
+    classical = int(9)
 
 def country_subclasses():
-	"love"
-    "hard_times"
-    "raisin’_hell"
-    "nostalgia"
-    "musical_aspirations"
-    "god/religion"
-    "the_south"
-    "america"
+	love = int(1)
+    hard_times = int(2)
+    raisin’_hell = int(3)
+    nostalgia = int(4)
+    musical_aspirations = int(5)
+    god/religion = int(6)
+    the_south = int(7)
+    america = int(8)
 
 def electronic_subclasses():
-    "electro"
-    "dubstep"
-    "techno"
-    "synth_pop"
-    "vaporwave"
-    "electroacoustic"
-    "breakbeat"
-    "industrial"
-    "breakbeat"
+    electro = int(1)
+    dubstep = int(2)
+    techno = int(3)
+    synth_pop = int(4)
+    vaporwave = int(5)
+    electroacoustic = int(6)
+    breakbeat = int(7)
+    industrial = int(8)
+    breakbeat = int(9)
 
 def folk_subclasses():
-    "bluegrass"
-    "gospel"
-    "old_time"
-    "jug_band"
-    "appalachian"
-    "blues_folk"
-    "cajun"
-    "native_american"
+    bluegrass = int(1)
+    gospel = int(2)
+    old_time = int(3)
+    jug_band = int(4)
+    appalachian = int(5)
+    blues_folk = int(6)
+    cajun = int(7)
+    native_american = int(8)
 
 def hip_hop_subclasses():
-    "lofi"
-    "trap"
-    "phonk"
-    "freestyle_rap"
-    "latin_hip"
-    "christian"
-    "crunk"
-    "gangster_rap"
+    lofi = int(1)
+    trap = int(2)
+    phonk = int(3)
+    freestyle_rap = int(4)
+    latin_hip = int(5)
+    christian = int(6)
+    crunk = int(7)
+    gangster_rap = int(8)
 
 def jazz_subclasses():
-    "ragtime"
-    "dixieland"
-    "swing"
-    "bebop"
-    "cool_jazz"
-    "hard_bop"
-    "modal"
-    "latin_jazz"
-    "fusion"
+    ragtime = int(1)
+    dixieland = int(2)
+    swing = int(3)
+    bebop = int(4)
+    cool_jazz = int(5)
+    hard_bop = int(6)
+    modal = int(7)
+    latin_jazz = int(8)
+    fusion = int(9)
 
 def reggae_subclasses():
-    "roots"
-    "dub"
-    "dancehall"
-    "lovers"
-    "raggamuffin"
-    "rock_ragg"
-    "reggae_fusion"
-    "two_tone"
-    "raggaeton"
-
-def reggae_subclasses():
-    "roots"
-    "dub"
-    "dancehall"
-    "lovers"
-    "raggamuffin"
-    "rock_ragg"
-    "reggae_fusion"
-    "two_tone"
-    "raggaeton"
+    roots = int(1)
+    dub = int(2)
+    dancehall = int(3)
+    lovers = int(4)
+    raggamuffin = int(5)
+    rock_ragg = int(6)
+    reggae_fusion = int(7)
+    two_tone = int(8)
+    raggaeton = int(9)
 
 def rock_subclasses():
-    "hardcore"
-    "classic_rock"
-    "punk_rock"
-    "alternative"
-    "indie"
-    "metal"
-    "folk_rock"
-    "progressive"
-    "electronic_rock"
+    hardcore = int(1)
+    classic_rock = int(2)
+    punk_rock = int(3)
+    alternative = int(4)
+    indie = int(5)
+    metal = int(6)
+    folk_rock = int(7)
+    progressive = int(8)
+    electronic_rock = int(9)
--- src/playlistinator/main.py
+++ src/playlistinator/main.py
@@ -1,3 +1,13 @@
-input(print(Welcome to the Playlistinator!  \
-\
-File Directory: \
+print(input("Welcome to the Playlistinator! Here you can organise your favorite songs into personal playlists!\
+File Directory (type the corresonding number): \
+blues = 1 \
+classical = 2 \
+country = 3 \
+electronic = 4 \
+folk = 5 \
+hip-hop = 6 \
+jazz = 7 \
+reggae = 8 \
+rock = 9 \
+genre: "))
+

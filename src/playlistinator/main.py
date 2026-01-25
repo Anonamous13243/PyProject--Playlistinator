63483283c173c696e838154528e906a246edf0c0
MagicG5509 <106779600+Anonamous13243@users.noreply.github.com>
2026-01-25 05:49:16 +0000

    changed strings to variables

--- src/playlistinator/main.py
+++ src/playlistinator/main.py
@@ -1,122 +1,122 @@
-input(print("Welcome to the Playlistinator! " \
+input(print(Welcome to the Playlistinator!  \
 \
-"File Directory:" \
-""))
+File Directory: \
+
 def main_genres():
-	str("blues")
-    str("classical")
-    str("country")
-    str("electronic")
-    str("folk")
-    str("hip_Hop")
-    str("jazz")
-    str("reggae")
-    str("rock")
+	var = blues
+    var = classical
+    var = country
+    var = electronic
+    var = folk
+    var = hip_Hop
+    var = jazz
+    var = reggae
+    var = rock
 
 def blues_subclasses():
-	str("country_blues")
-    str("delta_blues")
-    str("country")
-    str("piedmont_blues")
-    str("chicago_blues")
-    str("texas_blues")
-    str("west_coast_blues")
-    str("british_blues")
-    str("electric_blues")
+	var = country_blues
+    var = delta_blues
+    var = country
+    var = piedmont_blues
+    var = chicago_blues
+    var = texas_blues
+    var = west_coast_blues
+    var = british_blues
+    var = electric_blues
 
 def classical_subclasses():
-	str("medival")
-    str("renaissance")
-    str("baroque")
-    str("classical")
-    str("romantic")
-    str("modernism")
-    str("impressionism")
-    str("minimalism")
-    str("electronic classical")
+	var = medival
+    var = renaissance
+    var = baroque
+    var = classical
+    var = romantic
+    var = modernism
+    var = impressionism
+    var = minimalism
+    var = electronic classical
 
 def country_subclasses():
-	str("love")
-    str("hard_times")
-    str("raisin’_hell")
-    str("nostalgia")
-    str("musical_aspirations")
-    str("god/religion")
-    str("the_south")
-    str("america")
-    str("modes_of_transportation")
+	var = love
+    var = hard_times
+    var = raisin’_hell
+    var = nostalgia
+    var = musical_aspirations
+    var = god/religion
+    var = the_south
+    var = america
+    var = modes_of_transportation
 
 def electronic_subclasses():
-    "electro")
-    "dubstep")
-    "techno")
-    "synth_pop")
-    "vaporwave")
-    "electroacoustic")
-    "breakbeat")
-    "industrial")
-    "breakbeat")
+    var = electro
+    var = dubstep
+    var = techno
+    var = synth_pop
+    var = vaporwave
+    var = electroacoustic
+    var = breakbeat
+    var = industrial
+    var = breakbeat
 
 def folk_subclasses():
-    "bluegrass")
-    "gospel")
-    "old_time")
-    "jug_band")
-    "appalachian")
-    "blues_folk")
-    "cajun")
-    "native_american")
+    var = bluegrass
+    var = gospel
+    var = old_time
+    var = jug_band
+    var = appalachian
+    var = blues_folk
+    var = cajun
+    var = native_american
 
 def hip_hop_subclasses():
-    "lofi")
-    "trap")
-    "phonk")
-    "freestyle_rap")
-    "latin_hip")
-    "christian")
-    "crunk")
-    "gangster_rap")
+    var = lofi
+    var = trap
+    var = phonk
+    var = freestyle_rap
+    var = latin_hip
+    var = christian
+    var = crunk
+    var = gangster_rap
 
 def jazz_subclasses():
-    "ragtime"
-    "dixieland"
-    "swing"
-    "bebop"
-    "cool_jazz"
-    "hard_bop"
-    "modal"
-    "latin_jazz"
-    "fusion"
+    var = ragtime
+    var = dixieland
+    var = swing
+    var = bebop
+    var = cool_jazz
+    var = hard_bop
+    var = modal
+    var = latin_jazz
+    var = fusion
 
 def reggae_subclasses():
-    "roots"
-    "dub"
-    "dancehall"
-    "lovers"
-    "raggamuffin"
-    "rock_ragg"
-    "reggae_fusion"
-    "two_tone"
-    "raggaeton"
+    var = roots
+    var = dub
+    var = dancehall
+    var = lovers
+    var = raggamuffin
+    var = rock_ragg
+    var = reggae_fusion
+    var = two_tone
+    var = raggaeton
 
 def reggae_subclasses():
-    "roots"
-    "dub"
-    "dancehall"
-    "lovers"
-    "raggamuffin"
-    "rock_ragg"
-    "reggae_fusion"
-    "two_tone"
-    "raggaeton"
+    var = roots
+    var = dub
+    var = dancehall
+    var = lovers
+    var = raggamuffin
+    var = rock_ragg
+    var = reggae_fusion
+    var = two_tone
+    var = raggaeton
 
 def rock_subclasses():
-    "hardcore"
-    "classic_rock"
-    "punk_rock"
-    "alternative"
-    "indie"
-    "metal"
-    "folk_rock"
-    "progressive"
-    "electronic_rock"
+    var = hardcore
+    var = classic_rock
+    var = punk_rock
+    var = alternative
+    var = indie
+    var = metal
+    var = folk_rock
+    var = progressive
+    var = electronic_rock

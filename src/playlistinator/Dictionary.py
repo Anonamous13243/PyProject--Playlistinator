59aa5a5b30bba25efbe68946ffa2c487d503f304
MagicG5509 <106779600+Anonamous13243@users.noreply.github.com>
2026-01-26 16:39:00 -0800

    improved dictionary

--- src/playlistinator/Dictionary.py
+++ src/playlistinator/Dictionary.py
@@ -1,117 +1,127 @@
-def control():
-    float(1.0) = "1"
-    float(2.0) = "2"
-    float(3.0) = "3"
-    float(4.0) = "4"
-    float(5.0) = "5"
-    float(6.0) = "6"
-    float(7.0) = "7"
-    float(8.0) = "8"
-    float(9.0) = "9"
 
-def main_genres(input):
-	blues = float(1.0)
-    classical = float(2.0)
-    country = float(3.0)
-    electronic = float(4.0)
-    folk = float(5.0)
-    hip_Hop = float(6.0)
-    jazz = float(7.0)
-    reggae = float(8.0)
-    rock = float(9.0)
+main_genres = {
+    "blues": float(1.0),
+    "classical": float(2.0),
+    "country": float(3.0),
+    "electronic": float(4.0),
+    "folk": float(5.0),
+    "hip_Hop": float(6.0),
+    "jazz": float(7.0),
+    "reggae": float(8.0),
+    "rock": float(9.0),
+}
 
-def blues_subclasses(blues):
-	country_blues = float(1.1)
-    delta_blues = float(2.1)
-    country = float(3.1)
-    piedmont_blues = float(4.1)
-    chicago_blues = float(5.1)
-    texas_blues = float(6.1)
-    west_coast_blues = float(7.1)
-    british_blues = float(8.1)
-    electric_blues = float(9.1)
+print(main_genres)
 
-def classical_subclasses(classical):
-	medival = float(1.2)
-    renaissance = float(2.2)
-    baroque = float(3.2)
-    classical = float(4.2)
-    romantic = float(5.2)
-    modernism = float(6.2)
-    impressionism = float(7.2)
-    minimalism = float(8.2)
-    electronic = float(9.2)
+blues_subclasses ={
+    "country_blues": float(1.1),
+    "delta_blues": float(2.1),
+    "country": float(3.1),
+    "piedmont_blues": float(4.1),
+    "chicago_blues": float(5.1),
+    "texas_blues": float(6.1),
+    "west_coast_blues": float(7.1),
+    "british_blues": float(8.1),
+    "electric_blues": float(9.1),
+}
+    
 
-def country_subclasses(country):
-	love = float(1.3)
-    hard_times = float(2.3)
-    raisin’_hell = float(3.3)
-    nostalgia = float(4.3)
-    musical_aspirations = float(5.3)
-    god/religion = float(6.3)
-    the_south = float(7.3)
-    america = float(8.3)
+classical_subclasses = {
+    "medival": float(1.2),
+    "renaissance": float(2.2),
+    "baroque": float(3.2),
+    "classical": float(4.2),
+    "romantic": float(5.2),
+    "modernism": float(6.2),
+    "impressionism": float(7.2),
+    "minimalism": float(8.2),
+    "electronic": float(9.2),
+}
+   
 
-def electronic_subclasses(electronic):
-    electro = float(1.4)
-    dubstep = float(2.4)
-    techno = float(3.4)
-    synth_pop = float(4.4)
-    vaporwave = float(5.4)
-    electroacoustic = float(6.4)
-    breakbeat = float(7.4)
-    industrial = float(8.4)
-    breakbeat = float(9.4)
+country_subclasses = {
+    "love": float(1.3),
+    "hard_times": float(2.3),
+    "raisin_hell": float(3.3),
+    "nostalgia": float(4.3),
+    "musical_aspirations": float(5.3),
+    "god_or_religion": float(6.3),
+    "the_south": float(7.3),
+    "america": float(8.3),
+}
+    
 
-def folk_subclasses(folk):
-    bluegrass = float(1.5)
-    gospel = float(2.5)
-    old_time = float(3.5)
-    jug_band = float(4.5)
-    appalachian = float(5.5)
-    blues_folk = float(6.5)
-    cajun = float(7.5)
-    native_american = float(8.5)
+electronic_subclasses = {
+    "electro": float(1.4),
+    "dubstep": float(2.4),
+    "techno": float(3.4),
+    "synth_pop": float(4.4),
+    "vaporwave": float(5.4),
+    "electroacoustic": float(6.4),
+    "breakbeat": float(7.4),
+    "industrial": float(8.4),
+    "breakbeat": float(9.4),
+}
+    
+folk_subclasses = {
+    "bluegrass": float(1.5),
+    "gospel": float(2.5),
+    "old_time": float(3.5),
+    "jug_band": float(4.5),
+    "appalachian": float(5.5),
+    "blues_folk": float(6.5),
+    "cajun": float(7.5),
+    "native_american": float(8.5),
+}
+    
 
-def hip_hop_subclasses(hip_hop):
-    lofi = float(1.6)
-    trap = float(2.6)
-    phonk = float(3.6)
-    freestyle_rap = float(4.6)
-    latin_hip = float(5.6)
-    christian = float(6.6)
-    crunk = float(7.6)
-    gangster_rap = float(8.6)
+hip_hop_subclasses = {
+    "lofi": float(1.6),
+    "trap": float(2.6),
+    "phonk": float(3.6),
+    "freestyle_rap": float(4.6),
+    "latin_hip": float(5.6),
+    "christian": float(6.6),
+    "crunk": float(7.6),
+    "gangster_rap": float(8.6),
+}
+    
 
-def jazz_subclasses(jazz):
-    ragtime = float(1.7)
-    dixieland = float(2.7)
-    swing = float(3.7)
-    bebop = float(4.7)
-    cool_jazz = float(5.7)
-    hard_bop = float(6.7)
-    modal = float(7.7)
-    latin_jazz = float(8.7)
-    fusion = float(9.7)
+jazz_subclasses = {
+        "ragtime": float(1.7),
+    "dixieland": float(2.7),
+    "swing": float(3.7),
+    "bebop": float(4.7),
+    "cool_jazz": float(5.7),
+    "hard_bop": float(6.7),
+    "modal": float(7.7),
+    "latin_jazz": float(8.7),
+    "fusion": float(9.7),
+}
 
-def reggae_subclasses(reggae):
-    roots = float(1.8)
-    dub = float(2.8)
-    dancehall = float(3.8)
-    lovers = float(4.8)
-    raggamuffin = float(5.8)
-    rock_ragg = float(6.8)
-    reggae_fusion = float(7.8)
-    two_tone = float(8.8)
-    raggaeton = float(9.8)
 
-def rock_subclasses(rock):
-    hardcore = float(1.9)
-    classic_rock = float(2.9)
-    punk_rock = float(3.9)
-    alternative = float(4.9)
-    indie = float(5.9)
-    metal = float(6.9)
-    folk_rock = float(7.9)
-    progressive = float(8.9)
-    electronic_rock = float(9.9)
+reggae_subclasses = {
+    "roots": float(1.8),
+    "dub": float(2.8),
+    "dancehall": float(3.8),
+    "lovers": float(4.8),
+    "raggamuffin": float(5.8),
+    "rock_ragg": float(6.8),
+    "reggae_fusion": float(7.8),
+    "two_tone": float(8.8),
+    "raggaeton": float(9.8),
+}
+    
+
+rock_subclasses = {
+     "hardcore": float(1.9),
+    "classic_rock": float(2.9),
+    "punk_rock": float(3.9),
+    "alternative": float(4.9),
+    "indie": float(5.9),
+    "metal": float(6.9),
+    "folk_rock": float(7.9),
+    "progressive": float(8.9),
+    "electronic_rock": float(9.9),
+}
+   

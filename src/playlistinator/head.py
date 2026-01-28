bfadd6df6a61d926928e38af78a92c84f8ada9cb
MagicG5509 <106779600+Anonamous13243@users.noreply.github.com>
2026-01-28 19:35:37 +0000

    Simplified input

--- src/playlistinator/head.py
+++ src/playlistinator/head.py
@@ -3,15 +3,16 @@ if __name__ == "__main__":
     
     for interface, serial in main.items():
         print(input("Welcome to the Playlistinator! Here you can organise your favorite songs into personal playlists!\
-        File Directory (type the corresonding number): "))
-        print(main[interface])
-        if interface == serial:    
-            print(main[interface])
+            File Directory (type the corresonding number): "))
+        print("\
+        'blues': 1.(1-9),\
+        'classical': 2.(1-9),\
+        'country': 3.(1-9),\
+        'electronic': electronic,\
+        'folk': folk,\
+        'hip_hop': hip_hop,\
+        'jazz': jazz,\
+        'reggae': reggae,\
+        'rock': rock,")
         if interface != serial:
             print("Error! Given serial does not match corresponding dictionary cache.")
-
-    for serial in main.items:
-        print(serial)
-    
-    if input == serial:
-        print(interface)

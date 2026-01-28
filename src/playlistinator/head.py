c6531514c04dc4ee9ffc5d494b163f202c2f8c37
MagicG5509 <106779600+Anonamous13243@users.noreply.github.com>
2026-01-28 19:55:51 +0000

    improve system UI

--- src/playlistinator/head.py
+++ src/playlistinator/head.py
@@ -2,17 +2,19 @@ if __name__ == "__main__":
     from Dictionary import main
     
     for interface, serial in main.items():
-        print(input("Welcome to the Playlistinator! Here you can organise your favorite songs into personal playlists!\
-            File Directory (type the corresonding number): "))
-        print("\
-        'blues': 1.(1-9),\
-        'classical': 2.(1-9),\
-        'country': 3.(1-9),\
-        'electronic': electronic,\
-        'folk': folk,\
-        'hip_hop': hip_hop,\
-        'jazz': jazz,\
-        'reggae': reggae,\
-        'rock': rock,")
+        print(input("Welcome to the Playlistinator! Here you can organise your favorite songs into personal playlists! \n\
+        File Directory, type the corresonding number with 1.1 - 9.9: \n\
+        \n\
+        'blues': 1.(1-9),\n\
+        'classical': 2.(1-9),\n\
+        'country': 3.(1-9),\n\
+        'electronic': 4.(1-9),\n\
+        'folk': 5.(1-9),\n\
+        'hip_hop': 6.(1-9),\n\
+        'jazz': 7.(1-9),\n\
+        'reggae': 8.(1-9),\n\
+        'rock': 9.(1-9), \n\
+        \n\
+        Enter here: "))
         if interface != serial:
             print("Error! Given serial does not match corresponding dictionary cache.")

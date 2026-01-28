7cd40c4fc81508986e37e5cc9bbb82f15438c037
MagicG5509 <106779600+Anonamous13243@users.noreply.github.com>
2026-01-28 17:30:07 +0000

    Added moar to the control head

--- src/playlistinator/head.py
+++ src/playlistinator/head.py
@@ -1,17 +1,19 @@
 if __name__ == "__main__":
     from Dictionary import blues, classical, country, electronic, folk, hip_hop, jazz, reggae, rock
     
-    info_of_genres = blues.items.split, classical.items.split, country.items.split, electronic.items.split, folk.items.split, hip_hop.items.split, jazz.items.split, reggae.items.split, rock.items.split
+    info_of_genres = blues.items, classical.items, country.items, electronic.items, folk.items, hip_hop.items, jazz.items, reggae.items, rock.items
 
-    subgenre_interface = [str(info_of_genres)]
-    serial = [float(info_of_genres)]
+    subgenre_interface = []
+    serial = []
     for i in info_of_genres:
         subgenre_interface.append(i)
         serial.append(i)
-    print(subgenre_interface, serial)
-        
-
-'''
-print(input("Welcome to the Playlistinator! Here you can organise your favorite songs into personal playlists!\
-File Directory (type the corresonding number): {main_genres}"))
-'''
+    
+    print("Welcome to the Playlistinator! Here you can organise your favorite songs into personal playlists!\
+    File Directory (type the corresonding number): ")
+    print(subgenre_interface)
+    print(serial)
+    print(input("Enter here: "))
+    
+    if input == serial:
+        print("You selected {serial}")
